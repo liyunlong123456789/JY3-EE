c11d55109ecc53b86b3ff13c756d33b407b98506
liyunlong123456789 <17720079089@139.com>
2019-06-05 20:03:56 +0800

    20190605

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import com.neuedu.Maven.User;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("World");
+        User user = new User();
+
+    }
+}
